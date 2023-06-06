473ccda940046b6439f6d5e57530a3eecd9f1f75
Kzquandary <kzquandary@gmail.com>
2023-06-06 22:58:59 +0700

    Add Regex to Search and Change Event Handling

--- src/main/java/com/aslabapp/aslabapp/Main.java
+++ src/main/java/com/aslabapp/aslabapp/Main.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -25,6 +26,7 @@ import project.Route;
 
 import java.io.IOException;
 import java.util.Objects;
+
 public class Main extends Application {
 
     public Pane content;
@@ -34,6 +36,7 @@ public class Main extends Application {
     public Text jmllaporan;
     @FXML
     public static Rectangle background;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Home));
@@ -51,6 +54,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public void mainClose(MouseEvent mouseEvent) {
         Action action = new Action();
         action.handleExit(mouseEvent);
@@ -65,77 +69,101 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
     }
+
     public void home() {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.InfoHome));
         Action.Move(loader, content);
     }
+
     public void mahasiswa() throws IOException {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Mahasiswa));
         Action.Move(loader, content);
     }
+
     public void pertemuan() throws IOException {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Pertemuan));
         Action.Move(loader, content);
     }
+
     public void absensi() {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Absensi));
         Action.Move(loader, content);
     }
+
     public void laporan() throws IOException {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Laporan));
         Action.Move(loader, content);
     }
+
     public void nilai() {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Nilai));
         Action.Move(loader, content);
     }
+
     public void keaktifan() {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Keaktifan));
         Action.Move(loader, content);
     }
+
     public void setting() {
         content.getChildren().clear();
         FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Setting));
         Action.Move(loader, content);
     }
-    public void search_form() {
-        String searchText = search_form.getText();
-        content.getChildren().clear();
 
-        ImageView loadingImage = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("Loading.gif"))));
-        Action.LoadingAnimation(content, loadingImage);
+    public void carimahasiswa() {
+        if (!search_form.getText().isEmpty()) {
+            String searchText = search_form.getText();
+            if (searchText.matches("^[a-zA-Z0-9]+$")) {
 
-        Task<Void> searchTask = new Task<>() {
-            @Override
-            protected Void call() throws Exception {
-                Thread.sleep(1000); // Simulasi pencarian selama 1 detik
-                Platform.runLater(() -> {
-                    try {
-                        FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Search));
-                        Parent searchRoot = loader.load();
-                        SearchController searchController = loader.getController();
-                        searchController.setSearch(searchText);
-                        content.getChildren().clear();
-                        content.getChildren().add(searchRoot);
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                content.getChildren().clear();
+
+                ImageView loadingImage = new ImageView(new Image(Objects.requireNonNull(getClass().getResourceAsStream("Loading.gif"))));
+                Action.LoadingAnimation(content, loadingImage);
+
+                Task<Void> searchTask = new Task<>() {
+                    @Override
+                    protected Void call() throws Exception {
+                        Thread.sleep(1000); // Simulasi pencarian selama 1 detik
+                        Platform.runLater(() -> {
+                            try {
+                                FXMLLoader loader = new FXMLLoader(getClass().getResource(Route.Search));
+                                Parent searchRoot = loader.load();
+                                SearchController searchController = loader.getController();
+                                searchController.setSearch(searchText);
+                                content.getChildren().clear();
+                                content.getChildren().add(searchRoot);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+
+                        return null;
                     }
-                });
+                };
 
-                return null;
+                new Thread(searchTask).start();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Input tidak valid. Harap masukkan input yang sesuai dengan format.");
+                alert.showAndWait();
             }
-        };
-
-        new Thread(searchTask).start();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Silahkan isi form terlebih dahulu");
+            alert.showAndWait();
+        }
     }
-
-
 
 }
