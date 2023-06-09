222a4b923e238bf0fb9809e7599a66adae8c41b5
Kzquandary <kzquandary@gmail.com>
2023-06-09 22:18:05 +0700

    Add Login and Lock Route, Change Regex for NIM

--- src/main/java/project/Route.java
+++ src/main/java/project/Route.java
@@ -3,6 +3,8 @@ package project;
 public class Route {
     public static final String URL = "http://127.0.0.1:8000/api/";
     public static final String Home = "Home.fxml";
+    public static final String Login = "Login.fxml";
+    public static final String Lock = "Lock.fxml";
     public static final String Search = "Search.fxml";
     public static final String InfoHome = "InfoHome.fxml";
     public static final String Mahasiswa = "Mahasiswa.fxml";
@@ -11,11 +13,9 @@ public class Route {
     public static final String Laporan = "Laporan.fxml";
     public static final String Nilai = "Nilai.fxml";
     public static final String Keaktifan = "Keaktifan.fxml";
-
     public static final String Setting = "Setting.fxml";
-
     public static final String RegNama = "^(\\S+\\s){0,9}\\S+$";
-    public static final String RegNIM = "^3411211\\d+$";
+    public static final String RegNIM = "^3411[0-9][0-9]1\\d+$";
     public static final String RegNoHP = "^(62|0)\\d+$";
 
 }
